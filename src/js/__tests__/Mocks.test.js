8619036d7fba5e7857a3fb345aae3164474af9fd
VMoiseev <e175@inbox.ru>
2021-10-22 09:01:31 +0500

    initial commit

---
+++ src/js/__tests__/Mocks.test.js
@@ -0,0 +1,31 @@
+import GameSavingLoader from '../GameSavingLoader';
+import read from '../reader';
+
+jest.mock('../reader');
+
+afterEach(() => jest.resetAllMocks());
+
+test('Метод load должен создавать объект типа GameSaving', async () => {
+  read.mockReturnValue('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
+  const received = await GameSavingLoader.load();
+  const expected = {
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  };
+  expect(received).toEqual(expected);
+});
+
+test('Метод load должен выбрасывать ошибку', async () => {
+  read.mockReturnValue(new Error('Ошибка!'));
+  try {
+    await GameSavingLoader.load();
+  } catch (error) {
+    expect(error).toEqual('Ошибка!');
+  }
+});
